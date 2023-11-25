82124eb69135e9dea40d01dc7557a65bff9b2754
wsdwight1 <143677966+wsdwight1@users.noreply.github.com>
2023-11-24 19:47:14 -0800

    Update VendorHome.jsx

--- app/imports/ui/pages/VendorHome.jsx
+++ app/imports/ui/pages/VendorHome.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import '../../../client/style.css';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
 
-const VendorHome = () => (
-  <Container id="vendor-home" fluid className="py-3">
-    <Row>
-      <Col className="background-row d-flex flex-column align-items-center justify-content-center text-center">
-        <h1 className="h1-landing">Vendor Home Page</h1>
-        <p className="paragraph-landing">Welcome back, insertvendornamehere.</p>
-      </Col>
-    </Row>
-  </Container>
-);
+const VendorHome = ({ currentUser }) => {
+  // Destructure the username from currentUser
+  const { username } = currentUser || {};
 
-export default VendorHome;
+  return (
+    <Container id="vendor-home" fluid className="py-3">
+      <Row>
+        <Col className="background-row d-flex flex-column align-items-center justify-content-center text-center">
+          <h1 className="h1-landing">Vendor Home Page</h1>
+          <p className="paragraph-landing">
+            Welcome back, {username || 'Loading...'}.
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+VendorHome.propTypes = {
+  currentUser: PropTypes.shape({
+    username: PropTypes.string, // Assuming username is a string
+  }),
+};
+VendorHome.defaultProps = {
+  currentUser: null,
+};
+export default withTracker(() => ({
+  currentUser: Meteor.user(),
+}))(VendorHome);
