fbd164c1720f9034badd4b77ec55d6ae4945b591
wsdwight1 <143677966+wsdwight1@users.noreply.github.com>
2023-11-25 11:25:25 -0800

    Create EditVendor.jsx

---
+++ app/imports/ui/pages/EditVendor.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import swal from 'sweetalert';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { AutoForm, ErrorsField, HiddenField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import { useParams } from 'react-router';
+import { Stuffs } from '../../api/stuff/Stuff';
+import LoadingSpinner from '../components/LoadingSpinner';
+import { Vendors } from '../../api/vendors/Vendors';
+import { Foods } from '../../api/fooditems/Foods';
+
+const bridge = new SimpleSchema2Bridge(Stuffs.schema);
+
+/* Renders the EditStuff page for editing a single document. */
+const EditVendor = () => {
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const { _id } = useParams();
+  // console.log('EditStuff', _id);
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { doc, ready } = useTracker(() => {
+    // Get access to Stuff documents.
+    const subscriptionVendors = Meteor.subscribe(Vendors.vendorPublicationName);
+    const subscriptionFoods = Meteor.subscribe(Foods.vendorPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscriptionVendors.ready();
+    const rdyy = subscriptionFoods.ready();
+    // Get the document
+    const documentVendors = Vendors.collection.findOne(_id);
+    const documentFoods = Foods.collection.findOne(_id);
+    return {
+      doc: documentVendors, documentFoods,
+      ready: rdy, rdyy,
+    };
+  }, [_id]);
+  // console.log('EditStuff', doc, ready);
+  // On successful submit, insert the data.
+  const submit = (data) => {
+    const { name, quantity, condition } = data;
+    Stuffs.collection.update(_id, { $set: { name, quantity, condition } }, (error) => (error ?
+      swal('Error', error.message, 'error') :
+      swal('Success', 'Item updated successfully', 'success')));
+  };
+
+  return ready ? (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={5}>
+          <Col className="text-center"><h2>Edit Stuff</h2></Col>
+          <AutoForm schema={bridge} onSubmit={data => submit(data)} model={doc}>
+            <Card>
+              <Card.Body>
+                <TextField name="name" />
+                <NumField name="quantity" decimal={null} />
+                <SelectField name="condition" />
+                <SubmitField value="Submit" />
+                <ErrorsField />
+                <HiddenField name="owner" />
+              </Card.Body>
+            </Card>
+          </AutoForm>
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />;
+};
+
+export default EditVendor;
