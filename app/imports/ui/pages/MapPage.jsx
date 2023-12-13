022f0f7353140b89d1a6a517dd26382ebf9738a1
Joseph Heintz <heintzj@hawaii.edu>
2023-12-12 14:42:58 -1000

    zoomLevel adjustment

--- app/imports/ui/pages/MapPage.jsx
+++ app/imports/ui/pages/MapPage.jsx
@@ -43,7 +43,7 @@ const MapPage = () => {
           <Col className="text-center">
             <h1 className="h1-map">Map</h1>
           </Col>
-          {location && <Map location={location} zoomLevel={18} />}
+          {location && <Map location={location} zoomLevel={19} />}
         </Col>
       </Row>
     </Container>
